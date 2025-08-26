380309f054f619e103b761cffb03dc8d3c2fe886
Akira-Kiba-Nova <azhang17@huskers.unl.edu>
2025-08-26 11:19:43 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Argos Zhang
+ *
+ * Date: 2025/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Argos!\n");
+
+}
